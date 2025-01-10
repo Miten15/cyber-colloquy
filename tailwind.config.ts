cd41ebfaec135ed4c4fcae3ff5ebcd10f88231f9
Maheshannayboeina <mahesh.annayboeina18426@sakec.ac.in>
2025-01-10 19:07:27 +0530

    favicon error fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 
 const config: Config = {
-  mode: "jit",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
